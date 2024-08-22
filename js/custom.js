5fcbd28b5cc27a8f01e55b291e5c597dccf15624
qlsx2 <moon_a_h@naver.com>
2024-08-22 16:49:16 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('.to_top').on('click', function () {
+        $('html,body').animate({ scrollTop:0 }, 1000)
+    })
+});
+
+$(function () {
+    $('.main_visual_slide').slick({
+        arrows:false,
+        //dots: true,
+        autoplay: true,
+        pasueOnHover: false,
+});
+
+
+
+$('.main_visual_slide').on('afterChange', function (e, s, c) {
+    $('.main_visual .lnk li').removeClass('on');
+    $('.main_visual .lnk li').eq(c).addClass('on')
+    });
+
+})
